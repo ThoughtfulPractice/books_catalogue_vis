9406aaea6419b893efe99376926ea98ccf719493
ThoughtfulPractice <thoughtfulpracticesg@gmail.com>
2018-10-14 13:48:17 +0800

    Add tests for googlebooks_data.py and refactor to pass tests

--- src/data/googlebooks_data.py
+++ src/data/googlebooks_data.py
@@ -4,10 +4,15 @@ import requests
 import json
 import time
 from pathlib import Path
+import logging
 import os
 from dotenv import load_dotenv, find_dotenv
-import logging
+# find .env automagically by walking up directories until it's found
+dotenv_path = find_dotenv()
+# load environment variables
+load_dotenv(dotenv_path)
 
+GOOGLEBOOKS_API_KEY = os.getenv("GOOGLEBOOKS_API_KEY")
 
 # Setup filepaths
 project_dir = Path(__file__).resolve().parents[2]
@@ -18,19 +23,13 @@ raw_books_data_path = project_data_dir.joinpath(
 outfile_path = project_data_dir.joinpath(
     'external', 'googlebooks_volumes.json')
 
-# find .env automagically by walking up directories until it's found
-dotenv_path = find_dotenv()
-# load environment variables
-load_dotenv(dotenv_path)
-googlebooks_api_key = os.environ.get("GOOGLEBOOKS_API_KEY")
-
 
 @click.command()
 @click.option('--raw_books_data_path', default=raw_books_data_path,
               help='.csv path to raw_books_data')
 @click.option('--outfile_path', default=outfile_path,
               help='.json path to save googlebooks api data')
-@click.option('--googlebooks_api_key', default=googlebooks_api_key,
+@click.option('--googlebooks_api_key', default=GOOGLEBOOKS_API_KEY,
               help='your googlebooks api key')
 def main(raw_books_data_path, outfile_path, googlebooks_api_key):
 
@@ -41,29 +40,44 @@ def main(raw_books_data_path, outfile_path, googlebooks_api_key):
 
     # Load manually prepared books data
     books = pd.read_csv(raw_books_data_path)
-    books['NoDashISBN'] = books['ISBN'].apply(
-        lambda x: str(x.replace('-', '')))
+    books = add_nodashisbn_column(books)
 
     # Use ISBNs to get info on the books from GoogleBooks API
-    googlebooks_endpoint = "https://www.googleapis.com/books/v1/volumes"
-
-    googlebooks_volumes = []
-    for no_dash_isbn in books['NoDashISBN']:
-        payload = {'q': "isbn:" + no_dash_isbn,
-                   'key': googlebooks_api_key}
-        r = requests.get(googlebooks_endpoint, params=payload)
-        googlebooks_volumes.append(
-            {'no_dash_isbn': no_dash_isbn,
-             'response': r.json()}
-        )
-        time.sleep(0.2)
+    isbns = books['NoDashISBN'].tolist()
+    googlebooks_volumes = googlebooks_api(isbns, googlebooks_api_key)
 
     # Save as json file
-    with outfile_path.open('wb') as outfile:
+    with outfile_path.open('w') as outfile:
         json.dump(googlebooks_volumes, outfile,
                   ensure_ascii=False, indent=4, sort_keys=True)
     logger.info('Data saved to %s' % (outfile_path))
     logger.info('PROCESS COMPLETED')
+
+
+def googlebooks_api(isbns, googlebooks_api_key):
+    """Takes a list of ISBN number strings and returns json
+    data from googlebooks api"""
+    googlebooks_volumes = []
+    googlebooks_endpoint = "https://www.googleapis.com/books/v1/volumes"
+
+    no_dash_isbns = [str(x.replace('-', '')) for x in isbns]
+
+    for isbn in no_dash_isbns:
+        payload = {'q': "isbn:" + isbn,
+                   'key': googlebooks_api_key}
+        r = requests.get(googlebooks_endpoint, params=payload)
+        d = {'no_dash_isbn': isbn,
+             'response': r.json()}
+        googlebooks_volumes.append(d)
+        time.sleep(0.2)
+
+    return googlebooks_volumes
+
+
+def add_nodashisbn_column(df):
+    df['NoDashISBN'] = df['ISBN'].apply(
+        lambda x: str(x.replace('-', '')))
+    return df
 
 
 if __name__ == "__main__":
--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -1,7 +1,14 @@
 import pandas as pd
-from ...src.data import openlibrary_data
-import unittest
+from ...src.data import openlibrary_data, googlebooks_data
 import pytest
+import os
+from dotenv import load_dotenv, find_dotenv
+# find .env automagically by walking up directories until it's found
+dotenv_path = find_dotenv()
+# load environment variables
+load_dotenv(dotenv_path)
+
+GOOGLEBOOKS_API_KEY = os.getenv("GOOGLEBOOKS_API_KEY")
 
 # Tests for openlibrary_data.py
 @pytest.fixture(scope='session')
@@ -28,6 +35,36 @@ def test_adds_nodash_isbn_column_to_df(datadir):
     df = pd.read_csv(RAW_BOOKS_PATH)
 
     out = openlibrary_data.add_nodashisbn_column(df)
+    assert 'NoDashISBN' in out.columns
+    assert out['NoDashISBN'].equals((df['ISBN'].apply(
+        lambda x: str(x.replace('-', '')))))
+
+
+# Tests for googlebooks_data.py
+@pytest.fixture(scope='session')
+def googlebooks_api_response():
+    isbns = ["9780823014880", "fo49o28ytb26"]
+    response = googlebooks_data.googlebooks_api(isbns, GOOGLEBOOKS_API_KEY)
+    return response
+
+
+def test_googlebooks_api_returns_list(googlebooks_api_response):
+    assert isinstance(googlebooks_api_response, list)
+
+
+def test_googlebooks_api_returns_correct_number_of_responses(googlebooks_api_response):
+    assert len(googlebooks_api_response) == 2
+
+
+def assert_googlebooks_api_returns_isbns_without_dashes(googlebooks_api_response):
+    assert googlebooks_api_response[0]['no_dash_isbn'] == '9780823014880'
+
+
+def test_googlebooks_api_adds_nodash_isbn_column_to_df(datadir):
+    RAW_BOOKS_PATH = datadir["raw_books.csv"]
+    df = pd.read_csv(RAW_BOOKS_PATH)
+
+    out = googlebooks_data.add_nodashisbn_column(df)
     assert 'NoDashISBN' in out.columns
     assert out['NoDashISBN'].equals((df['ISBN'].apply(
         lambda x: str(x.replace('-', '')))))
